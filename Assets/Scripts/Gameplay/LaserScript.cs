0f07f097cad92a5c822c088faab7aa6fd6b7ae81
Lukas Prinz <prinz.lukas99@gmail.com>
2021-12-21 13:57:36 +0100

    Added Mechanic Destroy Laser and Enemy

--- Assets/Scripts/Gameplay/LaserScript.cs
+++ Assets/Scripts/Gameplay/LaserScript.cs
@@ -8,4 +8,14 @@ public class LaserScript : MonoBehaviour
     {
         GetComponent<Rigidbody>().velocity = Vector3.up * speed;
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(collision.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+
 }
